a3f2c72e63a3a92bf076af59ed33920c5ce5a3f3
gaojiuli <97676212@qq.com>
2016-12-30 15:06:36 +0800

    user password

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,8 @@
+import re
+
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 User = get_user_model()
 
@@ -23,8 +26,15 @@ class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('telephone',
-                  'is_admin',
                   'password')
+
+    def validate_telephone(self, value):
+        if not re.compile('^\d{11}$').match(value):
+            raise ValidationError("手机号码格式错误")
+        return value
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
@@ -43,3 +53,9 @@ class UserPartialUpdateSerializer(serializers.ModelSerializer):
                   'is_staff',
                   'is_admin',
                   'is_active')
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password')
+        instance.set_password(password)
+        instance.save(**validated_data)
+        return instance
